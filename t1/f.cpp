ac36d69648bb668041ec9b908fbe8f235dc3a877
Santos <santos.ag.dev@gmail.com>
2026-09-25 18:51:21 -0300

    Adicionar o primeiro slide e a primeira lista de exercícios

--- t1/f.cpp
+++ t1/f.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+using i32 = int;
+
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<i32> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
 }
