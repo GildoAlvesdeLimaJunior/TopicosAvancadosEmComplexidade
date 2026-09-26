5a3b5221baf79be3568c6c51058f3351337e1106
arthur-og <arthur12.2007g@gmail.com>
2026-09-26 13:18:40 -0300

    E almost solved,the generic case of triples is not solved yet

--- t1/e.cpp
+++ t1/e.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int k = 4;
+    unordered_map<int, int> map;
+    vector<int> v(n, 0);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+        map[v[i]] = map[v[i]] ? map[v[i]] + 1 : 1;
+    }
+    int duplas_que_anulam = 0;
+    int trios_que_anulam = 0;
+    for (auto e : map) {
+        if (map[-e.first]) {
+            duplas_que_anulam++;
+            if (map[e.first] == 3) {
+                trios_que_anulam++;
+            }
+        }
+    }
+    for (auto e : map) {
+        cout << " " << e.first << " " << e.second << endl;
+    }
+    if (duplas_que_anulam >= 2 || trios_que_anulam) {
+
+        cout << "Sim" << endl;
+    }
 }
